9f839d2d5e76de798ccccf26baadd82351003236
Matheus Lima Machado <matmachado@ni-58353-8p.uolcorp.intranet>
2020-05-19 08:31:59 -0400

    Fazendo uso de Reducer

--- reading-list/src/components/BookDetails.js
+++ reading-list/src/components/BookDetails.js
@@ -2,9 +2,12 @@ import React, { useContext } from "react";
 import { BookContext } from "../contexts/BookContext";
 
 const BookDetails = ({ book, background }) => {
-  const { removeBook } = useContext(BookContext);
+  const { dispatch } = useContext(BookContext);
   return (
-    <li onClick={() => removeBook(book.id)} style={{ background: background }}>
+    <li
+      onClick={() => dispatch({ type: "REMOVE_BOOK", id: book.id })}
+      style={{ background: background }}
+    >
       <div className="title">{book.title}</div>
       <div className="author">{book.author}</div>
     </li>
